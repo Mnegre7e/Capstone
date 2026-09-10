0e320707be6f5d992d7786c25534b3be19e74d63
RayCL2 <bast.bascunan@duocuc.cl>
2026-09-10 16:09:42 -0300

    #cambios minimos

--- HouseGreenM/src/app/pages/recuperar-clave/recuperar-clave.module.ts
+++ HouseGreenM/src/app/pages/recuperar-clave/recuperar-clave.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { IonicModule } from '@ionic/angular/lazy';
 
 import { RecuperarClavePageRoutingModule } from './recuperar-clave-routing.module';
 
@@ -11,7 +11,7 @@ import { RecuperarClavePage } from './recuperar-clave.page';
   imports: [
     CommonModule,
     FormsModule,
-
+    IonicModule,
     RecuperarClavePageRoutingModule
   ],
   declarations: [RecuperarClavePage],
