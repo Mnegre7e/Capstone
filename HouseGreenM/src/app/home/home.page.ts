04713458dec66a388a51e0c73b44b986ee21f488
RayCL2 <bast.bascunan@duocuc.cl>
2026-09-11 23:06:54 -0300

    Homepage completamente funcional en diseño faltan 8 vistas

--- HouseGreenM/src/app/home/home.page.ts
+++ HouseGreenM/src/app/home/home.page.ts
@@ -7,6 +7,7 @@ import { Browser } from '@capacitor/browser';
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  standalone: false
 })
 export class HomePage implements OnInit {
 
