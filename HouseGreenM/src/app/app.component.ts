1d4ad42b08a6798adda8497831ccaa8cb1184be9
RayCL2 <bast.bascunan@duocuc.cl>
2026-09-11 22:48:26 -0300

    #Se borrara la app para rehacerla desde 0

--- HouseGreenM/src/app/app.component.ts
+++ HouseGreenM/src/app/app.component.ts
@@ -7,7 +7,7 @@ register();
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false
+  standalone: true
 })
 export class AppComponent {
 
