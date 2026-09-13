b1f0c77efd27fc7814623c39b976ccee0a406dbe
RayCL2 <bast.bascunan@duocuc.cl>
2026-09-12 23:22:15 -0300

    configuracion del perfil

--- HouseGreenM/src/app/pages/perfil-configuracion/perfil-configuracion.module.ts
+++ HouseGreenM/src/app/pages/perfil-configuracion/perfil-configuracion.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular/lazy';
-
 import { PerfilConfiguracionPageRoutingModule } from './perfil-configuracion-routing.module';
-
 import { PerfilConfiguracionPage } from './perfil-configuracion.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
     PerfilConfiguracionPageRoutingModule
   ],
-  declarations: [PerfilConfiguracionPage]
+  declarations: [PerfilConfiguracionPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PerfilConfiguracionPageModule {}
--- HouseGreenM/src/app/pages/perfil-configuracion/perfil-configuracion.page.ts
+++ HouseGreenM/src/app/pages/perfil-configuracion/perfil-configuracion.page.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+export interface UsuarioPerfil {
+  nombre: string;
+  rut: string;
+  email: string;
+  telefono: string;
+  tipoUsuario: string;
+}
 
 @Component({
   selector: 'app-perfil-configuracion',
   templateUrl: './perfil-configuracion.page.html',
   styleUrls: ['./perfil-configuracion.page.scss'],
-  standalone: false,
+  standalone: false
 })
-export class PerfilConfiguracionPage implements OnInit {
+export class PerfilConfiguracionPage {
 
-  constructor() { }
+  private router = inject(Router);
 
-  ngOnInit() {
+  usuario: UsuarioPerfil = {
+    nombre: 'Juan Pérez',
+    rut: '12.345.678-9',
+    email: 'juan.perez@email.com',
+    telefono: '+56 9 8765 4321',
+    tipoUsuario: 'Inversionista'
+  };
+
+  irAFavoritos() {
+    this.router.navigate(['/catalogo-propiedades']);
   }
 
+  cerrarSesion() {
+    this.router.navigate(['/login']);
+  }
 }
